e555cc1c56fedb6365546d29bb4afddffce77c06
grimgrinn <grimgrinn@gmail.com>
2020-02-28 16:31:18 +0300

    Добавьте файлы проекта.

---
+++ graphics/graphics.cpp
@@ -0,0 +1,32 @@
+﻿#include "olcConsoleGameEngine.h":
+
+
+class ggEngine3d : public olcConsoleGameEngine
+{
+    public:
+        ggEngine3d() 
+        {
+            m_sAppName = L"3D Demo";
+        }
+
+    public: 
+        bool OnUserCreate() override
+        {
+            return true;
+        }
+
+        bool OnUserUpdate(float fElapsedTime) override
+        {
+            return true;
+        }
+};
+
+int main()
+{
+    ggEngine3d demo;
+
+    if (demo.ConstructConsole(256, 240, 4, 4))
+        demo.Start();
+
+    return true;
+}
